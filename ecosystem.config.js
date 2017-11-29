b4ff1b662f0836bc5875c8cef17e09fc14243ee1
Josh <joshmvince@gmail.com>
2017-11-29 11:39:28 +0000

    add pm2 for deploys

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'A Thousand Words',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-52-18-19-133.eu-west-1.compute.amazonaws.com',
+      key  : '~/.ssh/atw-ec2.pem',
+      ref  : 'origin/master',
+      repo : 'git@github.com:joshvince/atw_backend.git',
+      path : '/home/ubuntu/atw_backend',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+    }
+  }
+};
