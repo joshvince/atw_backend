24baa2fda234f7ed329c72c2da439e5aeac74245
Josh <joshmvince@gmail.com>
2018-02-19 16:32:21 +0000

    add endpoint to get picture by its id

--- picture/picture.js
+++ picture/picture.js
@@ -5,6 +5,18 @@ const PICTURES_TABLE_NAME = process.env.PICTURES_TABLE_NAME
 const db = require('../lib/db.js');
 const uuidv4 = require('uuid/v4');
 
+// Fetch one picture by its ID
+async function getOnePicture(pictureId) {
+  const primaryKey = {"uuid": pictureId}
+  try {
+    const item = await db.get(primaryKey, PICTURES_TABLE_NAME)
+    return {success: true, result: item.Item};
+  } catch (error) {
+    console.log(`There was an error getting the story: ${error}`)
+    return {success: false, result: error}
+  }
+}
+
 async function createPicture(params) {
   try {
     await db.create(params, PICTURES_TABLE_NAME)
@@ -51,6 +63,7 @@ function isValidPictureData(params) {
 }
 
 module.exports = {
+  getOnePicture: getOnePicture,
   create: createPicture,
   getAll: getAllPictures,
   getByUser: getPicturesByUser,
